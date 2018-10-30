7e1eca79c57fa9378a2c81619ccee126ce6b6182
BranimirMaximov <branimir.maximov@gmail.com>
2018-10-30 16:35:22 -0500

    Revert "So far of Fixing Indenting"
    This reverts commit e8743e89f9a497719343dd51eaa567c8094fa3e2.

--- simulator.py
+++ simulator.py
@@ -16,107 +16,108 @@ def disassemble(I,Nlines):
     
     	wrongOp = False
     	if (line == "\n"):
-            continue
+    		continue
     
     	line = line.replace("\n", "")
     	print ("Machine code: ", line)
     
     	op_bin = line[1:8]
     	if (op_bin == "0000000"):
-    	    op = "HLT"
-    	    output = op + " //Stop program\n" 
-    	    continue
+    		op = "HLT"
+    		output = op + " //Stop program\n" 
+    		continue
     
     	elif (op_bin == "1111100"):
-    	    op = "ADDN"
-    	    output = op + "//r3 = r3 - 1\n" 
-    	    continue
+    		op = "ADDN"
+    		output = op + "//r3 = r3 - 1\n" 
+    		continue
     
     	elif (op_bin == "0001000"):
-    	    op = "CNTR0"
-    	    output = op + " //Count the number of 1's in r0\n"
-    	    continue
+    		op = "CNTR0"
+    		output = op + " //Count the number of 1's in r0\n"
+    		continue
     	
     	op_bin = line[1:6]
     
     	if (op_bin == "00000"):
-    	    op = "SUBR0"
-    	    ry = str(int(line[6:8], 2)) 
-    	if (ry == '1'):
-            wrongOp = True
-    	if (wrongOp == False):
-    	    output = op + " r" + ry + " //r0 = r0 - ry\n"
-    	    continue
+    		op = "SUBR0"
+    		ry = str(int(line[6:8], 2)) 
+    		if (ry == '1'):
+    			wrongOp = True
+    		if (wrongOp == False):
+    			output = op + " r" + ry + " //r0 = r0 - ry\n"
+    			continue
+    
     	op_bin = line[1:5]
     	
     	if (op_bin == "0001"):
-    	    op = "XOR"
-    	    rx = str(int(line[5:6], 2))
-    	    ry = str(int(line[6:8], 2)) 
-    	    output = op + " r" + rx + ", r" + ry + " //rx = rx XOR ry\n"
-    	    continue
+    		op = "XOR"
+    		rx = str(int(line[5:6], 2))
+    		ry = str(int(line[6:8], 2)) 
+    		output = op + " r" + rx + ", r" + ry + " //rx = rx XOR ry\n"
+    		continue
     
     	elif (op_bin == "0000"):
-    	    op = "SLER"
-    	    rx = str(int(line[5:7], 2))
-    	    ry = str(int(line[7], 2))
-    	    output = op + " r" + rx + ", r" + ry + " //If rx < r0 then r3 = 1\n\t//Else r3 = 0\n"
-    	    continue
+    		op = "SLER"
+    		rx = str(int(line[5:7], 2))
+    		ry = str(int(line[7], 2))
+    		output = op + " r" + rx + ", r" + ry + " //If rx < r0 then r3 = 1\n\t//Else r3 = 0\n"
+    		continue
     
     	op_bin = line[1:4]
     
     	if (op_bin == "100"):
-    	    op = "ADD"	
-    	    rx = str(int(line[4:6], 2))
-    	    ry = str(int(line[6:8], 2))
-    	    output = op + " r" + rx + ", " + ry + " //rx = rx + ry\n"
-    	    continue
+    		op = "ADD"	
+    		rx = str(int(line[4:6], 2))
+    		ry = str(int(line[6:8], 2))
+    		output = op + " r" + rx + ", " + ry + " //rx = rx + ry\n"
+    		continue
     
     	elif (op_bin == "111"):
-    	    op = "ADDI"
-    	    rx = str(int(line[4:6], 2))
-    	    const = str(int(line[6:8], 2))
-    	    output = op + " r" + rx + ", " + const + " //rx = rx + imm\n"
-    	    continue
+    		op = "ADDI"
+    		rx = str(int(line[4:6], 2))
+    		const = str(int(line[6:8], 2))
+    		output = op + " r" + rx + ", " + const + " //rx = rx + imm\n"
+    		continue
     
     	elif (op_bin == "001"):
-    	    op = "LWD"
-    	    rx = str(int(line[4:6], 2))
-    	    ry = str(int(line[6:8], 2))
-    	    output = op + " r" + rx + ", r" + ry + " //rx = M[ry]\n"
-    	    continue
+    		op = "LWD"
+    		rx = str(int(line[4:6], 2))
+    		ry = str(int(line[6:8], 2))
+    		output = op + " r" + rx + ", r" + ry + " //rx = M[ry]\n"
+    		continue
     
     	elif (op_bin == "011"):
-    	    op = "SWD"
-    	    rx = str(int(line[4:6], 2))
-    	    ry = str(int(line[6:8], 2))
-    	    output = op + " r" + rx + ", r" + ry + " //M[ry] = rx\n"
-    	    continue
+    		op = "SWD"
+    		rx = str(int(line[4:6], 2))
+    		ry = str(int(line[6:8], 2))
+    		output = op + " r" + rx + ", r" + ry + " //M[ry] = rx\n"
+    		continue
     
     	elif (op_bin == "110"):
-    	    op = "SLE"
-    	    rx = str(int(line[4:6], 2))
-    	    ry = str(int(line[6:8], 2))
-    	    output = op + " r" + rx + ", " + ry + " //If rx < ry then r3 = 1\n\t//Else r3 = 0\n"
-    	    continue
+    		op = "SLE"
+    		rx = str(int(line[4:6], 2))
+    		ry = str(int(line[6:8], 2))
+    		output = op + " r" + rx + ", " + ry + " //If rx < ry then r3 = 1\n\t//Else r3 = 0\n"
+    		continue
     
     	elif (op_bin == "101"):
-    	    op = "INIT"
-    	    rx = str(int(line[4:6], 2))
-    	    const = str(int(line[6:8], 2))
-    	    output = op + " r" + rx + ", " + const + " //rx = imm\n"
-    	    continue
+    		op = "INIT"
+    		rx = str(int(line[4:6], 2))
+    		const = str(int(line[6:8], 2))
+    		output = op + " r" + rx + ", " + const + " //rx = imm\n"
+    		continue
     	
     	elif (op_bin == "010"):
-    	    op = "JIF"
-    	    sign = line[4] 
-    	    const = int(line[5:8], 2)
-    	    if (sign == '1'):
-    		const = -(0b111 - int(const) + 1)
+    		op = "JIF"
+    		sign = line[4] 
+    		const = int(line[5:8], 2)
+    		if (sign == '1'):
+    			const = -(0b111 - int(const) + 1)
     
-    	    const = str(const)
+    		const = str(const)
     
-    	    output = op + " " + const + " //If r3 = 1 then jump (PC = PC + imm)\n\t//Else do nothing\n"
+    		output = op + " " + const + " //If r3 = 1 then jump (PC = PC + imm)\n\t//Else do nothing\n"
     		
     	print (output)
     	output_file.write(output)
@@ -133,196 +134,196 @@ memory = []
 jumpMarkers = dict()
 
 def TwosComplement(num, numBits):
-    if (num < 0):
-        num = (1 << numBits) + num
-    else:
-    	if ((num & (1 << (numBits - 1))) != 0):
-            num -= (1 << numBits)
+	if (num < 0):
+		num = (1 << numBits) + num
+	else:
+		if ((num & (1 << (numBits - 1))) != 0):
+			num -= (1 << numBits)
 	
-    return num
+	return num
 
 
 for line in input_file1:
-    if (line == "\n"):
-    	continue
+	if (line == "\n"):
+		continue
 
-    line = line.replace("\n", "")
-    line = line.replace(" ", "")
+	line = line.replace("\n", "")
+	line = line.replace(" ", "")
 
-    line = line.split('/')
-    line = line[0]
+	line = line.split('/')
+	line = line[0]
 
-    if (line == "*instructions"):
-    	memSection = False
-    	codeSection = True
+	if (line == "*instructions"):
+		memSection = False
+		codeSection = True
 
-    if (memSection == True):
-	    line = line.split('~')
-	    if (line[2][0] == '-'):
-	    	line[2] = line[2].replace("-", "")
-	    	line[2] = 0b1111111111111111 - int(line[2]) + 1
+	if (memSection == True):
+		line = line.split('~')
+		if (line[2][0] == '-'):
+			line[2] = line[2].replace("-", "")
+			line[2] = 0b1111111111111111 - int(line[2]) + 1
 
-	    memValue = format(int(line[2]), "016b")
-	    memory.append(memValue)
+		memValue = format(int(line[2]), "016b")
+		memory.append(memValue)
 
-    if (line == "*memory"):
-    	memSection = True
+	if (line == "*memory"):
+		memSection = True
 
-    if (codeSection == True):
-    	line = line.replace("\t", "")
-    	line = line.replace("r", "")
-    	line = line.replace("[", "")
-    	line = line.replace("]", "")
+	if (codeSection == True):
+		line = line.replace("\t", "")
+		line = line.replace("r", "")
+		line = line.replace("[", "")
+		line = line.replace("]", "")
 		
-    if (line[0:3] == 'LWD'):
-        line = line.replace("LWD", "")
-        line = line.split(',')
+		if (line[0:3] == 'LWD'):
+            line = line.replace("LWD", "")
+            line = line.split(',')
             
-        op = "001"
+            op = "001"
             
-        rx = format(int(line[0]), "02b")
-        const = format(int(line[1]), "02b")
+            rx = format(int(line[0]), "02b")
+            const = format(int(line[1]), "02b")
             
-        output = op + rx + const
+            output = op + rx + const
 
-    elif (line[0:3] == 'SWD'):
-	line = line.replace("SWD", "")
-	line = line.split(',')
+		elif (line[0:3] == 'SWD'):
+			line = line.replace("SWD", "")
+			line = line.split(',')
 
-	op = "011"
-        rx = format(int(line[0]), "02b")
-	const = format(int(line[1]), "02b")
+			op = "011"
+			rx = format(int(line[0]), "02b")
+		 	const = format(int(line[1]), "02b")
 
-	output = op + rx + const
+			output = op + rx + const
 
-    elif (line[0:4] == 'SLER'):
-        line = line.replace("SLER", "")
-	line = line.split(',')
+		elif (line[0:4] == 'SLER'):
+			line = line.replace("SLER", "")
+			line = line.split(',')
 
-	op = "0000"
-	rx = format(int(line[0]), "02b")
-	ry = format(int(line[1]), "01b")
-	ry = ry[0]
+			op = "0000"
+			rx = format(int(line[0]), "02b")
+		 	ry = format(int(line[1]), "01b")
+			ry = ry[0]
 
-	output = op + rx + ry
+			output = op + rx + ry
 
-    elif (line[0:3] == 'SLE'):
-	line = line.replace("SLE", "")
-	line = line.split(',')
+		elif (line[0:3] == 'SLE'):
+			line = line.replace("SLE", "")
+			line = line.split(',')
 
-        op = "110"
-	rx = format(int(line[0]), "02b")
-	const = format(int(line[1]), "02b")
+			op = "110"
+			rx = format(int(line[0]), "02b")
+		 	const = format(int(line[1]), "02b")
 
-	output = op + rx + const
+			output = op + rx + const
 
-    elif (line[0:3] == 'SHL'):
-	line = line.replace("SHL", "")
-	line = line.split(',')
+		elif (line[0:3] == 'SHL'):
+			line = line.replace("SHL", "")
+			line = line.split(',')
 
-	op = "0001"
-	rx = format(int(line[0]), "02b")
-	const = format(int(line[1]), "01b")
-	const = const[0]
+			op = "0001"
+			rx = format(int(line[0]), "02b")
+		 	const = format(int(line[1]), "01b")
+			const = const[0]
 			
-	output = op + rx + const
+			output = op + rx + const
 
-    elif (line[0:4] == 'ADDN'):
-	line = line.replace("ADDN", "")
+		elif (line[0:4] == 'ADDN'):
+			line = line.replace("ADDN", "")
 
-	op = "1111"
+			op = "1111"
 
-	output = op + "100" 
+			output = op + "100" 
 
-    elif (line[0:4] == 'ADDI'):
-	    line = line.replace("ADDI", "")
-	    line = line.split(',')
+		elif (line[0:4] == 'ADDI'):
+			line = line.replace("ADDI", "")
+			line = line.split(',')
 	
-	    const = int(line[1])
-    if (const < 0):
-	    const *= -1
-	    const = 0b1111111111111111 - const + 1
-	    const = format(const, "02b")
-	    const = const[14:16]
-    else:
-        const = format(int(line[1]), "02b")
+			const = int(line[1])
+			if (const < 0):
+				const *= -1
+				const = 0b1111111111111111 - const + 1
+				const = format(const, "02b")
+				const = const[14:16]
+			else:
+				const = format(int(line[1]), "02b")
 
-	op = "111"
-	rx = format(int(line[0]), "02b")
-	output = op + rx + const
+			op = "111"
+			rx = format(int(line[0]), "02b")
+			output = op + rx + const
 
-    elif (line[0:3] == 'ADD'):
-	line = line.replace("ADD", "")
-	line = line.split(',')
+		elif (line[0:3] == 'ADD'):
+			line = line.replace("ADD", "")
+			line = line.split(',')
 
-	op = "100"
-	rx = format(int(line[0]), "02b")
-	ry = format(int(line[1]), "02b")
+			op = "100"
+			rx = format(int(line[0]), "02b")
+		 	ry = format(int(line[1]), "02b")
 
-	output = op + rx + ry
+			output = op + rx + ry
 
-    elif (line[0:4] == 'INIT'):
-	line = line.replace("INIT", "")
-	line = line.split(',')
+		elif (line[0:4] == 'INIT'):
+			line = line.replace("INIT", "")
+			line = line.split(',')
 
-	op = "101"
-	rx = format(int(line[0]), "02b")
-	ry = format(int(line[1]), "02b")
+			op = "101"
+			rx = format(int(line[0]), "02b")
+		 	ry = format(int(line[1]), "02b")
 
-	output = op + rx + ry
+			output = op + rx + ry
 
-    elif (line[0:3] == 'XOR'):
-	line = line.replace("XOR", "")
-	line = line.split(',')
+		elif (line[0:3] == 'XOR'):
+			line = line.replace("XOR", "")
+			line = line.split(',')
 
-	op = "0001"
-	rx = format(int(line[0]), "01b")
-	rx = rx[0]
-	ry = format(int(line[1]), "02b")
+			op = "0001"
+			rx = format(int(line[0]), "01b")
+			rx = rx[0]
+		 	ry = format(int(line[1]), "02b")
 
-	output = op + rx + ry
+			output = op + rx + ry
 
-    elif (line[0:3] == 'JIF'):
-	line = line.replace("JIF", "")
-	line = line.split(' ')
-	op = "010"
-	const = TwosComplement(int(line[0]), 4)
-	const = format(const, "04b")
+		elif (line[0:3] == 'JIF'):
+			line = line.replace("JIF", "")
+			line = line.split(' ')
+			op = "010"
+			const = TwosComplement(int(line[0]), 4)
+			const = format(const, "04b")
 
-	output = op + const
+			output = op + const
 
-    elif (line[0:5] == 'CNTR2'):
-	line = line.replace("CNTR2", "")
+		elif (line[0:5] == 'CNTR2'):
+			line = line.replace("CNTR2", "")
 
-	op = "0001"
+			op = "0001"
 
-	output = op + "000" 
+			output = op + "000" 
 
-    elif (line[0:5] == 'SUBR0'):
-	line = line.replace("SUBR0", "")
+		elif (line[0:5] == 'SUBR0'):
+			line = line.replace("SUBR0", "")
 
-	op = "0000"
-	ry = format(int(line[0]), "02b")
+			op = "0000"
+		 	ry = format(int(line[0]), "02b")
 
-	output = op + '0' + ry
+			output = op + '0' + ry
 
 
-    elif (line[0:3] == 'HLT'):
-	line = line.replace("HLT", "")
+		elif (line[0:3] == 'HLT'):
+			line = line.replace("HLT", "")
 
-	op = "000"
+			op = "000"
 
-	output = op + "00" + "00" 
+			output = op + "00" + "00" 
 
-	numOnes = output.count("1")
-    if ((numOnes % 2) == 0 and output != ""):
-	output = '0' + output
-    elif (output != ""):
-	output = '1' + output
+		numOnes = output.count("1")
+		if ((numOnes % 2) == 0 and output != ""):
+			output = '0' + output
+		elif (output != ""):
+			output = '1' + output
 
-    if (output != ""):
-	output_file.write(output + "\n")
-	output = ""
+		if (output != ""):
+			output_file.write(output + "\n")
+		output = ""
     
     
 def main():
